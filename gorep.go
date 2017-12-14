3757c423b0815e0d3c1efd1d07a3f75589864572
Iain Munro <iain17@me.com>
2017-12-14 02:48:20 +0100

    test directories :D

--- gorep.go
+++ gorep.go
@@ -6,7 +6,6 @@ import "fmt"
 
 import "strings"
 import "io/ioutil"
-import "regexp"
 import "os"
 import "github.com/Pallinder/go-randomdata"
 
@@ -36,69 +35,34 @@ func main() {
 		flagPath, _ = os.Getwd()
 	}
 
-	regexImport, err := regexp.Compile(`(?s)(import(.*?)\)|import.*$)`)
-	if err != nil {
-		fmt.Println("ERROR", err.Error())
-		return
-	}
-
-	regexImportedPackage, err := regexp.Compile(`"(.*?)"`)
-	if err != nil {
-		fmt.Println("ERROR", err.Error())
-		return
-	}
-
 	found := []string{}
 
-	err = filepath.Walk(flagPath, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(flagPath, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			for ban, replace := range bans {
 				if strings.Contains(info.Name(), ban) {
 					os.Rename(path, strings.Replace(path, ban, replace, -1))
 				}
 			}
+			return nil
 		}
-		if filepath.Ext(path) == ".go" {
-			bts, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
 
-			content := string(bts)
-			matches := regexImport.FindAllString(content, -1)
-			isExists := false
+		bts, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
 
-		isReplacable:
-			for _, each := range matches {
-				for _, eachLine := range strings.Split(each, "\n") {
-					matchesInline := regexImportedPackage.FindAllString(eachLine, -1)
-					if err != nil {
-						return err
-					}
+		content := string(bts)
 
-					for _, eachSubline := range matchesInline {
-						for ban, _ := range bans {
-							if strings.Contains(eachSubline, ban) {
-								isExists = true
-								break isReplacable
-							}
-						}
-					}
-				}
-			}
+		for ban, replace := range bans {
+			content = strings.Replace(content, ban, replace, -1)
+			content = strings.Replace(content, ban, replace, -1)
+		}
+		found = append(found, path)
 
-			if isExists {
-				for ban, replace := range bans {
-					content = strings.Replace(content, ban, replace, -1)
-					content = strings.Replace(content, ban, replace, -1)
-				}
-				found = append(found, path)
-			}
-
-			err = ioutil.WriteFile(path, []byte(content), info.Mode())
-			if err != nil {
-				return err
-			}
+		err = ioutil.WriteFile(path, []byte(content), info.Mode())
+		if err != nil {
+			return err
 		}
 
 		return nil
